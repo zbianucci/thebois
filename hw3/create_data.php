d7a71a541978eb2497f5f28f23b747dbe3a7de12
zbianucci Github <zachbianucci@gmail.com>
2021-09-06 00:16:34 -0400

    Finished the output of the table and customers.txt file. Note that the customers.txt file is not included in the github but is intended to be created when the code is ran.

--- hw3/create_data.php
+++ hw3/create_data.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
-
+<head>
+<title>Your Array</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Chatta-Soup-A</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="style.css" />
+</head>
 <body>
-    <?php 
+    <?php
 
     //Array of first names
     $first_names = fopen("first_names.csv", "r");
@@ -24,10 +31,10 @@
     $last_names = fopen("last_names.txt", "r");
     $last_names_array = array();
 
-    while(!feof($last_names)) {
-        $lastname= fgets($last_names);
+    while (!feof($last_names)) {
+        $lastname = fgets($last_names);
 
-        if(!feof($last_names)){
+        if (!feof($last_names)) {
             $last_names_array[] = $lastname;
         }
     }
@@ -38,7 +45,7 @@
     echo "<pre>";
     print_r($last_names_array);
     echo "</pre>";
-    
+
     fclose($last_names);
     //End of last names
 
@@ -46,13 +53,13 @@
     $street_names = fopen("street_names.txt", "r");
     $street_names_string = '';
 
-    while(!feof($street_names)){
+    while (!feof($street_names)) {
         $street_names_string .= fgets($street_names);
     }
 
     $street_names_array = explode(":", $street_names_string);
     $street_names_array = str_replace(array("\n", "\r"), '', $street_names_array);
-    
+
     echo "Street names";
     echo "<pre>";
     print_r($street_names_array);
@@ -82,9 +89,9 @@
     $first_domains_array = str_replace(array("\n", "\r"), '', $first_domains_array);
     $domains_array = array();
 
-    
-    for ($i = 0; $i < count($first_domains_array); $i+=2) {
-        $domains_array[] = $first_domains_array[$i].".".$first_domains_array[$i+1];
+
+    for ($i = 0; $i < count($first_domains_array); $i += 2) {
+        $domains_array[] = $first_domains_array[$i] . "." . $first_domains_array[$i + 1];
     }
 
     echo "Domains";
@@ -96,8 +103,39 @@
     //End of domain names
 
     //Arrays: $first_names_array, $last_names_array, $street_names_array, $street_types_array, $domains_array
-    //START HERE
+    //Copied and changed the code for our table from hw2
+
+    //Table headers
+    echo "<table><tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Address</th>
+    <th>Email</th></tr>";
+
+    //table contents
+    $customers = fopen("customers.txt", "w");
+    for ($i = 0; $i < 25; $i++) {
+        $fname = $first_names_array[$i];
+        $lname = $last_names_array[$i];
+        $address = ($i+1).rand(1,99)." ".$street_names_array[$i]." ".$street_types_array[rand(0,count($street_types_array)-1)];
+        $email = $first_names_array[$i].".".$last_names_array[$i]."@".$domains_array[rand(0,count($domains_array)-1)];
+        
+        //output to the customers.txt file
+        fwrite($customers,$fname.":".$lname.":".$address.":".$email."\n");
+
+        //output to the table
+        echo "<tr>
+        <td>".$fname."</td>
+        <td>".$lname."</td>
+        <td>".$address."</td>
+        <td>".$email."</td>
+        </tr>";
+        }
+
     
+
+    fclose($customers);
+    echo "</table>";
     ?>
 </body>
 
