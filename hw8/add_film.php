8213bf9d45be11a247218c8062c4254510ea666a
zbianucci Github <zachbianucci@gmail.com>
2021-10-24 15:44:41 -0400

    completed task 2

---
+++ hw8/add_film.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Your Array</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Homework 8</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <div class="header">
+        <div class="center">
+            <h1>Homework 8</h1>
+        </div>
+        <div class="center">
+            <h2>Abiezer DeJesus & Zach Bianucci</h2>
+        </div>
+    </div>
+
+    <div class="page-wrapper">
+
+        <?php
+        //START PHP CODE HERE
+            ?>
+
+    </div>
+</body>
+
+</html>
+
+</html>
